20bc73a7627ff304bf9895c20d5d9324327058e7
haitamft <118185349+haitamft@users.noreply.github.com>
2023-03-26 22:09:04 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,941 @@
+const reservations = [
+    {
+      
+      resCheckIn: "2023-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-03-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-05-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-08-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-09-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-11-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-12-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-08-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-09-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-11-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-12-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-11-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-12-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-03-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-03-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-08-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-09-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-11-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-12-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-05-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-05-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-12-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-11-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-03-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-08-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-09-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2023--14",
+      
+    },
+
+
+
+
+    {
+      
+      resCheckIn: "2024-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-03-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-05-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-08-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-09-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-11-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-12-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-08-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-09-14",
+      
+    },
+    {
+      
+      resCheckIn: "2024-11-14",
+      
+    },
+
+
+
+
+    {
+      
+      resCheckIn: "2025-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-05-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-12-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-11-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-03-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-08-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-09-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2025--14",
+      
+    },
+
+
+    
+
+    {
+      
+      resCheckIn: "2026-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-05-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-08-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-09-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-11-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-12-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-08-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-09-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-11-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-12-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-11-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-12-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-03-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-03-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-08-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-09-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-11-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-12-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2026-05-14",
+      
+    },
+
+
+
+
+
+
+
+    {
+      
+      resCheckIn: "2027-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-08-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-09-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-10-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027--14",
+      
+    },
+
+
+
+
+    {
+      
+      resCheckIn: "2027-01-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-02-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-03-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-04-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-05-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-06-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-07-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-08-14",
+      
+    },
+    {
+      
+      resCheckIn: "2027-09-14",
+      
+    },
+    
+  ];
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const countsByYear = {};
+
+  reservations.forEach((reservation) => {
+    const date = new Date(reservation.resCheckIn);
+    const year = date.getFullYear();
+    const month = date.getMonth();
+
+    if (!countsByYear[year]) {
+      countsByYear[year] = {};
+    }
+
+    if (countsByYear[year][month]) {
+      countsByYear[year][month]++;
+    } else {
+      countsByYear[year][month] = 1;
+    }
+  });
+
+  let selectedYear = new Date().getFullYear();
+
+  const yearOptions = [];
+  for (let i = selectedYear; i <= selectedYear + 10; i++) {
+    yearOptions.push(`<option value="${i}">${i}</option>`);
+  }
+
+  const data = {
+    labels: months,
+    datasets: [
+      {
+        label: `Reservations per Month (${selectedYear})`,
+        data: Object.values(countsByYear[selectedYear] || {}),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "Number of Reservations",
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          text: "Month",
+        },
+      },
+    },
+  };
+
+  const chart = new Chart(document.getElementById("myChart"), {
+    type: "bar",
+    data: data,
+    options: options,
+  });
+
+  const yearSelect = document.getElementById("yearSelect");
+  yearSelect.innerHTML = yearOptions.join("");
+  yearSelect.value = selectedYear;
+
+      yearSelect.addEventListener("change", (event) => {
+    selectedYear = parseInt(event.target.value);
+
+    chart.options.plugins.title.text = `Reservations per Month (${selectedYear})`;
+    chart.data.datasets[0].data = Object.values(
+      countsByYear[selectedYear] || {}
+    );
+    chart.update();
+  });
